317c5650d5f3545e095687db639809d66686d899
Eli Duke <elimduke@gmail.com>
2017-07-27 17:51:59 -0700

    Uploads development images to /testing folder

--- app/models/manual.rb
+++ app/models/manual.rb
@@ -2,10 +2,14 @@ class Manual < ApplicationRecord
   has_permalink :slug
 
   has_attached_file :photo,
-    path: '/:class/:permalink',
+    path: '/:folder/:permalink',
     styles: { square: "500x500#" }
 
   validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
+
+  Paperclip.interpolates :folder do |attachment, style|
+    attachment.instance.folder
+  end
 
   Paperclip.interpolates :permalink do |attachment, style|
     attachment.instance.permalink + "-#{style}" + File.extname(attachment.original_filename)
@@ -13,5 +17,9 @@ class Manual < ApplicationRecord
 
   def slug
     "#{carrier}-#{make}-#{model}"
+  end
+
+  def folder
+    Rails.env == "development" ? "testing" : "manuals"
   end
 end
