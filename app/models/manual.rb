9d9c6829e004dfc939b22b8ed4d7ff5637651d2c
Eli Duke <elimduke@gmail.com>
2017-07-27 17:34:12 -0700

    Adds 'square' attachment style

--- app/models/manual.rb
+++ app/models/manual.rb
@@ -1,10 +1,14 @@
 class Manual < ApplicationRecord
   has_permalink :slug
-  has_attached_file :photo, path: '/:class/:permalink'
+
+  has_attached_file :photo,
+    path: '/:class/:permalink',
+    styles: { square: "500x500#" }
+
   validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
 
   Paperclip.interpolates :permalink do |attachment, style|
-    attachment.instance.permalink + File.extname(attachment.original_filename)
+    attachment.instance.permalink + "-#{style}" + File.extname(attachment.original_filename)
   end
 
   def slug
