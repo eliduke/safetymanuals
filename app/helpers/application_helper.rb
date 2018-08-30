9c9d4b221072d6a204dd8c6adb4e9b5e717f68ed
Eli Duke <elimduke@gmail.com>
2018-08-29 18:50:37 -0700

    Cleans up styles and adds error messages on the form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def error_messages_for(object, attribute, alignment="right")
+    if object.errors.messages[attribute].present?
+      messages = object.errors.full_messages_for(attribute).join(", ")
+      content_tag :p, messages, class: "error-messages-for text-#{alignment}"
+    end
+  end
 end
